7d7c1399f6066cdba0b8469dd49f5a58b68f5237
Mayank Singh <msingh@cogitate.us>
2023-07-24 01:06:47 +0530

    redux setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+import {createStore} from 'redux';
+
+const store = createStore(movies);
+
+console.log('store', store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
